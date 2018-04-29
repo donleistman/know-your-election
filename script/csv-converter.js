63fd608e48008888a586e1bc6fedacfd5c9c163d
Don Leistman <donleistman@gmail.com>
2018-04-29 03:07:38 -0400

    improve csv-converter

--- script/csv-converter.js
+++ script/csv-converter.js
@@ -2,29 +2,56 @@ const csv = require('csvtojson');
 const csvFilePath = '/Users/Don/dev/fullstack/mapper/script/seed-data/1976.csv';
 
 const fs = require('fs');
-let outputPath = '/Users/Don/dev/fullstack/mapper/script/seed-data/1976.json';
+let outputPath = '/Users/Don/dev/fullstack/mapper/script/seed-data/1976.js';
 
-csv({ delimiter: [','] })
-  .fromFile(csvFilePath)
-  .on('json', row => {
-    let text;
-    const { year, stateId, name, ev, c1, c2,
-      c1Id, c1name, c1party, c2Id, c2name, c2party } = row;
-    if (name === 'CANDIDATES') {
-      text = JSON.stringify({ c1Id, c1name, c1party }) + ',';
-      text += JSON.stringify({ c2Id, c2name, c2party }) + ',';
-    } else {
-      text = JSON.stringify({ year, stateId, name, ev, votesWon: c1, candidateId: c1Id }) + ',';
-      text += JSON.stringify({ year, stateId, name, ev, votesWon: c2, candidateId: c2Id }) + ',';
-    }
+let electionYear = 'YEAR';
+let startText = `const _${electionYear} = { candidates: [`;
 
-    fs.appendFile(outputPath, text, (err) => {
-      if (err) throw err;
-      console.log('wrote to file');
+fs.appendFile(outputPath, startText, (err) => {
+  if (err) throw err;
+  console.log('wrote start text');
+
+  csv({ delimiter: [','] })
+    .fromFile(csvFilePath)
+    .on('json', row => {
+
+      let text;
+      const { year, stateId, name, ev, c1, c2,
+        c1Id, c1name, c1party, c2Id, c2name, c2party } = row;
+      if (name === 'CANDIDATES') {
+        text = JSON.stringify({ id: c1Id, name: c1name, party: c1party, year }) + ',';
+        text += JSON.stringify({ id: c2Id, name: c2name, party: c2party, year }) + ',';
+        text += '  ], states: [';
+      } else {
+        text = JSON.stringify({
+          year, name, ev,
+          stateId: stateId === '' ? null : stateId,
+          votesWon: c1 === '-' ? null : c1,
+          candidateId: c1Id
+        }) + ',';
+        text += JSON.stringify({
+          year, name, ev,
+          stateId: stateId === '' ? null : stateId,
+          votesWon: c2 === '-' ? null : c2,
+          candidateId: c2Id
+        }) + ',';
+      }
+
+      fs.appendFile(outputPath, text, (err) => {
+        if (err) throw err;
+        console.log('wrote to file');
+      });
+
+    })
+    .on('done', error => {
+      if (error) console.log(error);
+
+      const endText = `] };\n module.exports = _${electionYear};`;
+      fs.appendFile(outputPath, endText, (err) => {
+        if (err) throw err;
+        console.log('wrote end text');
+        console.log('end');
+      });
     });
+});
 
-  })
-  .on('done', error => {
-    if (error) console.log(error);
-    console.log('end');
-  });
